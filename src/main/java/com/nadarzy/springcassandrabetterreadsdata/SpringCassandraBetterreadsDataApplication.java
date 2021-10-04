b8f769f090fca28dc5df6cd33bc540d7d87a9286
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-10-04 14:55:50 +0200

    Initial commit

---
+++ src/main/java/com/nadarzy/springcassandrabetterreadsdata/SpringCassandraBetterreadsDataApplication.java
@@ -0,0 +1,13 @@
+package com.nadarzy.springcassandrabetterreadsdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCassandraBetterreadsDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCassandraBetterreadsDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nadarzy/springcassandrabetterreadsdata/SpringCassandraBetterreadsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadarzy.springcassandrabetterreadsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCassandraBetterreadsDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
