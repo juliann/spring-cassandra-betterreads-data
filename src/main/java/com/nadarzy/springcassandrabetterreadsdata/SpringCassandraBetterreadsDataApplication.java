e8085fac473cc02e6234e1161e08bea6b50acd63
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-10-21 11:39:31 +0200

    reading from authors.txt, parsing and creating simple author object and store in cassandra working

--- src/main/java/com/nadarzy/springcassandrabetterreadsdata/SpringCassandraBetterreadsDataApplication.java
+++ src/main/java/com/nadarzy/springcassandrabetterreadsdata/SpringCassandraBetterreadsDataApplication.java
@@ -3,7 +3,10 @@ package com.nadarzy.springcassandrabetterreadsdata;
 import com.nadarzy.springcassandrabetterreadsdata.author.Author;
 import com.nadarzy.springcassandrabetterreadsdata.author.AuthorRepository;
 import com.nadarzy.springcassandrabetterreadsdata.connection.DataStaxAstraProperties;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -11,11 +14,21 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class SpringCassandraBetterreadsDataApplication {
+
+  @Value("${datadump.location.author}")
+  private String authorDumpLocation;
+
+  @Value("${datadump.location.works}")
+  private String worksDumpLocation;
 
   @Autowired AuthorRepository authorRepository;
 
@@ -25,12 +38,40 @@ public class SpringCassandraBetterreadsDataApplication {
 
   @PostConstruct
   public void start() {
-    System.out.println("application started");
-    Author author = new Author();
-    author.setId("id");
-    author.setName("name");
-    author.setPersonalName("personal name");
-    authorRepository.save(author);
+    initAuthors();
+    initWorks();
+  }
+
+  private void initWorks() {}
+
+  private void initAuthors() {
+    Path path = Paths.get(authorDumpLocation);
+    try (Stream<String> lines = Files.lines(path); ) {
+      lines
+          .limit(10)
+          .forEach(
+              line -> {
+                // read and parse the line
+                String jsonString = line.substring(line.indexOf("{"));
+                JSONObject jsonObject = null;
+                try {
+                  jsonObject = new JSONObject(jsonString);
+
+                  // create author object
+                  Author author = new Author();
+                  author.setName(jsonObject.optString("name"));
+                  author.setPersonalName(jsonObject.optString("personal_name"));
+                  author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+                  // persist to cassandra
+                  authorRepository.save(author);
+                } catch (JSONException e) {
+                  e.printStackTrace();
+                }
+              });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Bean
